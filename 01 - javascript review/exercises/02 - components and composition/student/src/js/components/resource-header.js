e7dafaa5fe0ed2dff50cb945df0f2ec972ef656f
katelyster <k.lyster@outlook.com>
2026-01-27 13:22:44 -0700

    wrap-up of components

--- 01 - javascript review/exercises/02 - components and composition/student/src/js/components/resource-header.js
+++ 01 - javascript review/exercises/02 - components and composition/student/src/js/components/resource-header.js
@@ -1,30 +1,28 @@
- const template = document.createElement("template");
- template.innerHTML = `
- <header class="mb-4">
-      <div class="d-flex flex-wrap justify-content-between align-items-end gap-2">
+const template = document.createElement("template");
+template.innerHTML = `
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <header class="mb-4">
+        <div class="d-flex flex-wrap justify-content-between align-items-end gap-2">
         <div>
-          <h1 class="h3 mb-1">NAIT Resource Directory</h1>
-          <p class="text-body-secondary mb-0">
+            <h1 class="h3 mb-1">NAIT Resource Directory</h1>
+            <p class="text-body-secondary mb-0">
             Find student support services, labs, and campus resources.
-          </p>
+            </p>
         </div>
-      </div>
+        </div>
     </header>
- `
-
- class ResourceHeader extends HTMLelement {
-      connectedCallback() { // ie. "at the end of loading this into the DOM,
-        this.attachShadow({mode: 'open'}) // create a shadow root; make it accessible
+`
+// N.B. it is a better pattern to create & attach to the shadow root in the constructor,
+//      rather than the connectedCallback (i.e. waiting until the component is loaded to the DOM).
+//      *this specific* example does the shadow root logic in the connectedCallback simply to keep it concise and legible
+//      as an introductory example.
+class ResourceHeader extends HTMLElement {
+    connectedCallback() {  // i.e. "at the end of *loading this into* the DOM, 
+        this.attachShadow({mode: 'open'})  // create a shadow root; make it accessible outside this component
         this.shadowRoot.appendChild(template.content.cloneNode(true))
-        // cloning a node just creates a copy. the idea is that we want to keep our template in tact. this is important for components with *open* shaow roots 
-      }
- }
+        // cloning a node just creates a copy; the idea is that we want to keep our template intact.
+        // this is especially important for components with *open* shadow roots, i.e. mutable by outside logic/JS
+    }
+}
 
- customElements.define('resource-header', ResourceHeader);
-
- // it is a better pattern to create and attach to the shadow root in the constructor, rather than the connectedCallback (ie. waiting until the component is loaded to the DOM)
-
- // this specific example 
-
- 
- 
+customElements.define('resource-header', ResourceHeader);
